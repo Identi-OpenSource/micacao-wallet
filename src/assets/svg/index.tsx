8544c222659b85a5ddd975d8368b2278c3dade7f
jroman-identi <jroman@identi-digital.org>
2024-04-02 16:30:27 -0500

    style: add svgs ans changes in labels

--- src/assets/svg/index.tsx
+++ src/assets/svg/index.tsx
@@ -1,2 +1,6 @@
-export {default as Iamfrom_w}  from './iamfrom_w.svg'
-export {default as Iamfrom_m}  from './iamfrom_m.svg'
+export { default as Iamfrom_w } from "./iamfrom_w.svg";
+export { default as Iamfrom_m } from "./iamfrom_m.svg";
+export { default as Logo } from "./logo.svg";
+export { default as Woman } from "./woman.svg";
+export { default as Man } from "./man.svg";
+export { default as Welcome_W } from "./welcome_W.svg";
