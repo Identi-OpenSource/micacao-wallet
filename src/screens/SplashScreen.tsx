4c22e8348e422849fedb228ee176f63b0e0146ef
jroman-identi <jroman@identi-digital.org>
2024-04-16 13:12:06 -0500

    feat: fix splash screen

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react";
-import { View, Image, StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { Logo } from "../assets/svg/index";
+import { COLORS_DF, FONT_FAMILIES } from "../config/themes/default";
 interface SplashScreenProps {
   navigation: any;
 }
-
 const SplashScreen: React.FC<SplashScreenProps> = ({ navigation }) => {
   useEffect(() => {
     setTimeout(() => {
@@ -14,10 +14,12 @@ const SplashScreen: React.FC<SplashScreenProps> = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Logo width={205} height={205}></Logo>
+      <Logo width={100} height={92.06} />
+      <Text style={styles.textSplash}>MiCacao</Text>
     </View>
   );
 };
+FONT_FAMILIES;
 
 const styles = StyleSheet.create({
   container: {
@@ -25,6 +27,11 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#F6EEEB",
+  },
+  textSplash: {
+    fontFamily: FONT_FAMILIES.bold,
+    color: COLORS_DF.citrine_brown,
+    fontSize: 32,
   },
 });
 
