a0fe5e4c1a2b9612cd4820af0e1814f671b465ae
jroman-identi <jroman@identi-digital.org>
2024-04-22 18:26:33 -0500

    feat: add changes for save in context the pin

--- src/screens/public/register/RegisterFourthScreen.tsx
+++ src/screens/public/register/RegisterFourthScreen.tsx
@@ -4,65 +4,69 @@
  * @summary : Register screen of the application
  */
 
-import {Field, Formik} from 'formik'
-import React, {useContext} from 'react'
-import {View} from 'react-native'
-import {sha256} from 'react-native-sha256'
-import {Password_M, Password_W} from '../../../assets/svg'
-import {Btn} from '../../../components/button/Button'
-import {SafeArea} from '../../../components/safe-area/SafeArea'
-import {storage} from '../../../config/store/db'
-import {LABELS} from '../../../config/texts/labels'
-import {UsersContext} from '../../../states/UserContext'
+import { Field, Formik } from "formik";
+import React, { useContext } from "react";
+import { View } from "react-native";
+import { sha256 } from "react-native-sha256";
+import { Password_M, Password_W } from "../../../assets/svg";
+import { Btn } from "../../../components/button/Button";
+import { SafeArea } from "../../../components/safe-area/SafeArea";
+import { storage } from "../../../config/store/db";
+import { LABELS } from "../../../config/texts/labels";
+import { UsersContext, UserDispatchContext } from "../../../states/UserContext";
 import {
   INIT_VALUES_FOURTH,
   INPUTS_FOURTH,
   InterfaceFourth,
   SCHEMA_FOURTH,
-} from './Interfaces'
-import {Header} from './RegisterScreen'
-import {styles} from './styles'
+} from "./Interfaces";
+import { Header } from "./RegisterScreen";
+import { styles } from "./styles";
 
 interface RegisterFourthScreenProps {
-  navigation: any
+  navigation: any;
 }
 const RegisterFourthScreen: React.FC<RegisterFourthScreenProps> = ({
   navigation,
 }) => {
   const submit = (values: InterfaceFourth) => {
     sha256(values.pin)
-      .then(pinHash => {
-        storage.set('security', JSON.stringify({pin: pinHash}))
-        navigation.navigate('ConfirmPasswordScreen', {pin: pinHash})
+      .then((pinHash) => {
+        storage.set("security", JSON.stringify({ pin: pinHash }));
+        dispatch({ type: "setUser", payload: { ...user, pin: pinHash } });
+        navigation.navigate("ConfirmPasswordScreen", { pin: pinHash });
+        console.log(user);
       })
-      .catch(error => {
-        console.log(error)
-      })
-  }
-  const user = useContext(UsersContext)
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const user = useContext(UsersContext);
+  const dispatch = useContext(UserDispatchContext);
   return (
     <SafeArea bg="isabelline" isForm>
       <View style={styles.container}>
-        <Header navigation={navigation} title={''} />
-        {user.gender == 'M' && <Password_M />}
-        {user.gender == 'W' && <Password_W />}
+        <Header navigation={navigation} title={""} />
+        {user.gender == "M" && <Password_M />}
+        {user.gender == "W" && <Password_W />}
 
         <Formik
           initialValues={INIT_VALUES_FOURTH}
-          onSubmit={values => submit(values)}
-          validationSchema={SCHEMA_FOURTH}>
-          {({handleSubmit, isValid, dirty}) => (
+          onSubmit={(values) => submit(values)}
+          validationSchema={SCHEMA_FOURTH}
+        >
+          {({ handleSubmit, isValid, dirty }) => (
             <>
               <View style={styles.formContainer}>
                 <View style={styles.formInput}>
-                  {INPUTS_FOURTH.map(i => (
+                  {INPUTS_FOURTH.map((i) => (
                     <Field key={i.name} {...i} />
                   ))}
                 </View>
                 <View style={styles.formBtn}>
                   <Btn
                     title={LABELS.confirm}
-                    theme={isValid && dirty ? 'agrayu' : 'agrayuDisabled'}
+                    theme={isValid && dirty ? "agrayu" : "agrayuDisabled"}
                     onPress={handleSubmit}
                     disabled={!isValid || !dirty}
                   />
@@ -73,6 +77,6 @@ const RegisterFourthScreen: React.FC<RegisterFourthScreenProps> = ({
         </Formik>
       </View>
     </SafeArea>
-  )
-}
-export default RegisterFourthScreen
+  );
+};
+export default RegisterFourthScreen;
