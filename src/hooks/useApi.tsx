95be485924d8652310fea7075cc053f7e0ced6c9
jroman-identi <jroman@identi-digital.org>
2024-04-23 09:57:16 -0500

    feat: add context for sales and add hook api create sale

--- src/hooks/useApi.tsx
+++ src/hooks/useApi.tsx
@@ -1,74 +1,103 @@
-import {useContext} from 'react'
-import Config from 'react-native-config'
-import {storage} from '../config/store/db'
-import {API_INTERFACE, HTTP} from '../services/api'
-import {AuthContext, useAuth} from '../states/AuthContext'
-import {UsersContext, parcelContext} from '../states/UserContext'
-import {SyncDataContext} from '../states/SyncDataContext'
-import useInternetConnection from './useInternetConnection'
+import { useContext } from "react";
+import Config from "react-native-config";
+import { storage } from "../config/store/db";
+import { API_INTERFACE, HTTP } from "../services/api";
+import { AuthContext, useAuth } from "../states/AuthContext";
+import {
+  CacaoContext,
+  UsersContext,
+  parcelContext,
+} from "../states/UserContext";
+import { SyncDataContext } from "../states/SyncDataContext";
+import useInternetConnection from "./useInternetConnection";
 
 const useApi = () => {
-  const {setToken} = useAuth()
-  const {isConnected} = useInternetConnection()
-  const accessToken = useContext(AuthContext)
-  const parcel = useContext(parcelContext)
-  const syncData = useContext(SyncDataContext)
-  const {addToSync} = syncData
+  const { setToken } = useAuth();
+  const { isConnected } = useInternetConnection();
+  const accessToken = useContext(AuthContext);
+  const parcel = useContext(parcelContext);
+  const syncData = useContext(SyncDataContext);
+  const sale = useContext(CacaoContext);
+  const { addToSync } = syncData;
   const createProducer = async (key: string) => {
-    console.log('createProducer')
+    console.log("createProducer");
 
     if (isConnected) {
       try {
-        const user = JSON.parse(storage.getString(key) || '{}')
+        const user = JSON.parse(storage.getString(key) || "{}");
         const apiRequest: API_INTERFACE = {
           url: `${Config.BASE_URL}/create_producer`,
-          method: 'POST',
+          method: "POST",
           payload: {
             dni: user.dni,
             name: user.name,
             phone: user.phone,
-            gender: user.gender == 'M' ? 'MALE' : 'FEMALE',
-            countryid: user.country?.code === 'CO' ? 1 : 2,
+            gender: user.gender == "M" ? "MALE" : "FEMALE",
+            countryid: user.country?.code === "CO" ? 1 : 2,
           },
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
             Authorization: `Bearer ${accessToken}`,
           },
-        }
-        const data = await HTTP(apiRequest)
-        console.log('data', data)
-        addToSync(JSON.stringify({...user, syncUp: true}), key)
+        };
+        const data = await HTTP(apiRequest);
+        console.log("data", data);
+        addToSync(JSON.stringify({ ...user, syncUp: true }), key);
       } catch (error) {
-        console.log('error', error)
+        console.log("error", error);
       }
     }
-  }
+  };
 
   const createFarm = async () => {
-    const parcel = useContext(parcelContext)
+    const parcel = useContext(parcelContext);
     if (isConnected) {
       try {
         const apiRequest: API_INTERFACE = {
           url: `${Config.BASE_URL}/create_farm`,
-          method: 'POST',
+          method: "POST",
           payload: {
             parcel: parcel.nameParcel,
             hectares: parcel.hectares,
           },
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
             Authorization: `Bearer ${accessToken}`,
           },
-        }
-        const data = await HTTP(apiRequest)
-        console.log('data', data)
+        };
+        const data = await HTTP(apiRequest);
+        console.log("data", data);
       } catch (error) {
-        console.log('error', error)
+        console.log("error", error);
       }
     }
-  }
+  };
+  const createSale = async () => {
+    const sale = useContext(CacaoContext);
+    if (isConnected) {
+      try {
+        const apiRequest: API_INTERFACE = {
+          url: `${Config.BASE_URL}/create_activities`,
+          method: "POST",
+          payload: {
+            typeCacao: sale.typeCacao,
+            kg: sale.kgCacao,
+            month: sale.month,
+          },
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${accessToken}`,
+          },
+        };
+        const data = await HTTP(apiRequest);
+        console.log("data", data);
+      } catch (error) {
+        console.log("error", error);
+      }
+    }
+  };
 
-  return {createProducer, createFarm}
-}
+  return { createProducer, createFarm, createSale };
+};
 
-export default useApi
+export default useApi;
