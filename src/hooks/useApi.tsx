967162cd4536561def3552c721c81956804d8d23
jroman-identi <jroman@identi-digital.org>
2024-04-29 23:29:13 -0500

    feat: add changes for addtosync

--- src/hooks/useApi.tsx
+++ src/hooks/useApi.tsx
@@ -1,92 +1,88 @@
-import {useContext} from 'react'
-import Config from 'react-native-config'
-import Toast from 'react-native-toast-message'
-import {storage} from '../config/store/db'
-import {API_INTERFACE, HTTP} from '../services/api'
-import {useAuth} from '../states/AuthContext'
-import useSync from './useSyncData'
-import {CacaoContext, parcelContext} from '../states/UserContext'
+import Config from "react-native-config";
+import { storage } from "../config/store/db";
+import { API_INTERFACE, HTTP } from "../services/api";
+import { useAuth } from "../states/AuthContext";
 
 const useApi = (setLoadingSync: any, setErrorSync: any, addToSync: any) => {
-  const {accessToken} = useAuth()
+  const { accessToken } = useAuth();
 
   const createProducer = async (key: string) => {
-    setLoadingSync(true)
+    setLoadingSync(true);
     try {
-      const user = JSON.parse(storage.getString(key) || '{}')
+      const user = JSON.parse(storage.getString(key) || "{}");
       const apiRequest: API_INTERFACE = {
         url: `${Config.BASE_URL}/create_producer`,
-        method: 'POST',
+        method: "POST",
         payload: {
           dni: user.dni,
           name: user.name,
           phone: user.phone,
-          gender: user.gender == 'M' ? 'MALE' : 'FEMALE',
-          countryid: user.country?.code === 'CO' ? 1 : 2,
+          gender: user.gender == "M" ? "MALE" : "FEMALE",
+          countryid: user.country?.code === "CO" ? 1 : 2,
         },
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
           Authorization: `Bearer ${accessToken}`,
         },
-      }
-      const data = await HTTP(apiRequest)
-      console.log('data', data)
-      addToSync(JSON.stringify({...user, syncUp: true}), key)
+      };
+      const data = await HTTP(apiRequest);
+      console.log("data", data);
+      addToSync(JSON.stringify({ ...user, syncUp: true }), key);
     } catch (error) {
       if (error?.response?.data) {
-        const errorText = JSON.stringify(error.response.data.errors)
-        setErrorSync(errorText)
+        const errorText = JSON.stringify(error.response.data.errors);
+        setErrorSync(errorText);
       } else {
-        setErrorSync(error)
+        setErrorSync(error);
       }
     } finally {
-      setLoadingSync(false)
-      setErrorSync(null)
+      setLoadingSync(false);
+      setErrorSync(null);
     }
-  }
+  };
 
   const createFarm = async () => {
-    const parcels = JSON.parse(storage.getString('parcels') || '[]')
+    const parcels = JSON.parse(storage.getString("parcels") || "[]");
 
     try {
       const apiRequest: API_INTERFACE = {
         url: `${Config.BASE_URL}/create_farm`,
-        method: 'POST',
+        method: "POST",
         payload: {
           farm_name: parcels.name,
           hectares: parcels.hectares,
         },
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
           Authorization: `Bearer ${accessToken}`,
         },
-      }
-      const data = await HTTP(apiRequest)
-      console.log('data', data)
+      };
+      const data = await HTTP(apiRequest);
+      console.log("data", data);
     } catch (error) {
-      console.log('error', error)
+      console.log("error", error);
     }
-  }
+  };
 
   const createSale = async () => {
-    const saleTemp = JSON.parse(storage.getString('saleTemp') || '{}')
+    const saleTemp = JSON.parse(storage.getString("saleTemp") || "{}");
     try {
       const apiRequest: API_INTERFACE = {
         url: `${Config.BASE_URL}/create_activities`,
-        method: 'POST',
+        method: "POST",
         payload: {
           cacao_type: saleTemp.type,
           dry_weight: saleTemp.kl,
         },
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
           Authorization: `Bearer ${accessToken}`,
         },
-      }
-      const data = await HTTP(apiRequest)
-      console.log('data', data)
+      };
+      const data = await HTTP(apiRequest);
+      console.log("data", data);
     } catch (error) {
-      console.log('error', error)
+      console.log("error", error);
       /* if (error.response.data) {
         const errorText = JSON.stringify(error.response.data.errors);
 
@@ -97,9 +93,9 @@ const useApi = (setLoadingSync: any, setErrorSync: any, addToSync: any) => {
       //setLoadingSync(false);
       //setErrorSync(null);
     }
-  }
+  };
 
-  return {createProducer, createFarm, createSale}
-}
+  return { createProducer, createFarm, createSale };
+};
 
-export default useApi
+export default useApi;
--- src/screens/private/parcel/draw-polygon/GradientLineRecorrer.tsx
+++ src/screens/private/parcel/draw-polygon/GradientLineRecorrer.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import {
   Camera,
   LineLayer,
@@ -5,190 +6,187 @@ import {
   PointAnnotation,
   ShapeSource,
   StyleURL,
-} from '@rnmapbox/maps'
-import {Alert, StatusBar, View} from 'react-native'
-import React, {
-  useState,
-  useRef,
-  ComponentProps,
-  useMemo,
-  forwardRef,
-  useEffect,
-} from 'react'
-import {storage} from '../../../../config/store/db'
-import Geolocation from 'react-native-geolocation-service'
-import {Btn, BtnIcon} from '../../../../components/button/Button'
-import {useNavigation} from '@react-navigation/native'
-import {COLORS_DF, MP_DF} from '../../../../config/themes/default'
-import ModalComponent from '../../../../components/modalComponent'
-type Position = [number, number]
+} from "@rnmapbox/maps";
+import React, { useEffect, useMemo, useRef, useState, useContext } from "react";
+import { Alert, StatusBar, View } from "react-native";
+import Geolocation from "react-native-geolocation-service";
+import { Btn, BtnIcon } from "../../../../components/button/Button";
+import ModalComponent from "../../../../components/modalComponent";
+import { storage } from "../../../../config/store/db";
+import { COLORS_DF, MP_DF } from "../../../../config/themes/default";
+import { SyncDataContext } from "../../../../states/SyncDataContext";
+
+type Position = [number, number];
 
 const lineLayerStyle = {
-  lineColor: '#fff',
+  lineColor: "#fff",
   lineWidth: 3,
-}
+};
 
-const Polygon = ({coordinates}: {coordinates: Position[]}) => {
+const Polygon = ({ coordinates }: { coordinates: Position[] }) => {
   const features: GeoJSON.FeatureCollection = useMemo(() => {
     return {
-      type: 'FeatureCollection',
+      type: "FeatureCollection",
       features: [
         {
-          type: 'Feature',
-          id: 'a-feature',
+          type: "Feature",
+          id: "a-feature",
           geometry: {
-            type: 'LineString',
+            type: "LineString",
             coordinates,
           },
           properties: {},
         } as const,
       ],
-    }
-  }, [coordinates])
+    };
+  }, [coordinates]);
   // console.log('=> features', JSON.stringify(features))
   return (
-    <ShapeSource id={'shape-source-id-0'} shape={features}>
-      <LineLayer id={'line-layer'} style={lineLayerStyle} />
+    <ShapeSource id={"shape-source-id-0"} shape={features}>
+      <LineLayer id={"line-layer"} style={lineLayerStyle} />
     </ShapeSource>
-  )
-}
+  );
+};
 
 const GradientLineRecorrer = () => {
-  const parcel = JSON.parse(storage.getString('parcels') || '[]')
+  const parcel = JSON.parse(storage.getString("parcels") || "[]");
   const firstPoint = [
     Number(parcel[0].firstPoint[1]),
     Number(parcel[0].firstPoint[0]),
-  ] as Position
-  const [coordinates, setCoordinates] = useState<Position[]>([firstPoint])
-  const [cam, setCam] = useState<Position>(firstPoint)
-  const [started, setStarted] = useState(false)
-  const navigation = useNavigation()
-  const [showModal, setShowModal] = useState(false)
-  const coordinatesWithLast = useMemo(() => {
-    return [...coordinates]
-  }, [coordinates])
+  ] as Position;
+  const syncData = useContext(SyncDataContext);
+  const { hasDataToSync, addToSync, toSyncData, dataToSync } = syncData;
+  const [coordinates, setCoordinates] = useState<Position[]>([firstPoint]);
+  const [cam, setCam] = useState<Position>(firstPoint);
+  const [started, setStarted] = useState(false);
+  const navigation = useNavigation();
+  const [showModal, setShowModal] = useState(false);
 
-  const map = useRef<MapView>(null)
+  const coordinatesWithLast = useMemo(() => {
+    return [...coordinates];
+  }, [coordinates]);
+
+  const map = useRef<MapView>(null);
 
   useEffect(() => {
-    let watchId: any = null
+    let watchId: any = null;
     if (started) {
       watchId = Geolocation.watchPosition(
-        position => {
-          console.log('=> position', position)
+        (position) => {
+          console.log("=> position", position);
 
           setCoordinates((prevPositions: any[]) => {
             const DATA = [
               ...prevPositions,
               [position.coords.longitude, position.coords.latitude],
-            ]
-            storage.set('polygonTemp', JSON.stringify(DATA))
-            return DATA
-          })
-          setCam([position.coords.longitude, position.coords.latitude])
+            ];
+            storage.set("polygonTemp", JSON.stringify(DATA));
+            return DATA;
+          });
+          setCam([position.coords.longitude, position.coords.latitude]);
         },
-        error => {
-          console.log(error)
+        (error) => {
+          console.log(error);
         },
         {
           enableHighAccuracy: true,
           distanceFilter: 1,
           interval: 1000,
-        },
-      )
+        }
+      );
     }
 
     return () => {
-      watchId !== null && Geolocation.clearWatch(watchId)
-    }
-  }, [started])
+      watchId !== null && Geolocation.clearWatch(watchId);
+    };
+  }, [started]);
 
   useEffect(() => {
     // eliminar polygonTemp
     // storage.delete('polygonTemp')
 
     if (parcel[0].polygon) {
-      setCoordinates(parcel[0].polygon)
+      setCoordinates(parcel[0].polygon);
     } else {
-      if (storage.getString('polygonTemp')) {
+      if (storage.getString("polygonTemp")) {
         const coordinateTemp = JSON.parse(
-          storage.getString('polygonTemp') || '',
-        )
-        setCoordinates(coordinateTemp)
+          storage.getString("polygonTemp") || ""
+        );
+        setCoordinates(coordinateTemp);
       }
     }
-  }, [])
+  }, []);
 
   const onSubmit = () => {
     if (coordinatesWithLast.length < 5) {
-      Alert.alert('Error', 'El polígono debe tener al menos 4 puntos')
-      return
+      Alert.alert("Error", "El polígono debe tener al menos 4 puntos");
+      return;
     }
     // Guardar en la lista de polígonos
     const newParcel = {
       ...parcel[0],
       polygon: [...coordinatesWithLast, coordinatesWithLast[0]],
       syncUp: false,
-    }
+    };
 
-    //AddtoSync
-
-    storage.set('parcels', JSON.stringify([newParcel]))
-    storage.delete('polygonTemp')
-    setShowModal(true)
-  }
+    addToSync("parcels", JSON.stringify([newParcel]));
+    storage.delete("polygonTemp");
+    setShowModal(true);
+  };
   const closeModal = () => {
-    setShowModal(false)
-    navigation.navigate('DrawPolygonScreen')
-  }
+    setShowModal(false);
+    navigation.navigate("DrawPolygonScreen");
+  };
   const deletePoint = () => {
     if (coordinates.length > 1) {
-      setCoordinates(prev => {
-        const newCoordinates = prev.slice(0, -1)
-        storage.set('polygonTemp', JSON.stringify(newCoordinates))
-        return newCoordinates
-      })
+      setCoordinates((prev) => {
+        const newCoordinates = prev.slice(0, -1);
+        storage.set("polygonTemp", JSON.stringify(newCoordinates));
+        return newCoordinates;
+      });
     }
-  }
+  };
 
   return (
-    <View style={{flex: 1}}>
-      <View style={{flex: 1}}>
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
         <StatusBar backgroundColor="#8F3B06" barStyle="light-content" />
         <ModalComponent
           isVisible={showModal}
-          label={'¡El mapa de tu parcela ha sido guardado!'}
+          label={"¡El mapa de tu parcela ha sido guardado!"}
           closeModal={closeModal}
-          buttonText={'Continuar'}
+          buttonText={"Continuar"}
         />
         <MapView
           ref={map}
           styleURL={StyleURL.Satellite}
-          style={{flex: 1}}
+          style={{ flex: 1 }}
           scaleBarEnabled={false}
           rotateEnabled={false}
           attributionEnabled={false}
           compassEnabled={false}
-          logoEnabled={false}>
+          logoEnabled={false}
+        >
           {<Polygon coordinates={coordinatesWithLast} />}
           {coordinatesWithLast.map((c, i) => {
             // buscar ultimo index en coordinates
-            const lastIndex = coordinates.length - 1
+            const lastIndex = coordinates.length - 1;
             return (
               <PointAnnotation
                 key={i.toString() + coordinates.length}
                 id={i.toString()}
-                coordinate={[c[0], c[1]]}>
+                coordinate={[c[0], c[1]]}
+              >
                 <View
                   style={{
                     height: 10,
                     width: 10,
-                    backgroundColor: lastIndex === i ? 'red' : 'white',
+                    backgroundColor: lastIndex === i ? "red" : "white",
                     borderRadius: 5,
                   }}
                 />
               </PointAnnotation>
-            )
+            );
           })}
           <Camera
             defaultSettings={{
@@ -200,47 +198,49 @@ const GradientLineRecorrer = () => {
         </MapView>
         <View
           style={{
-            position: 'absolute',
+            position: "absolute",
             bottom: MP_DF.large,
-            width: '100%',
+            width: "100%",
             minHeight: 50,
             paddingHorizontal: MP_DF.large,
             zIndex: 1000000,
-          }}>
+          }}
+        >
           <View
             style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
+              flexDirection: "row",
+              justifyContent: "space-between",
               marginBottom: MP_DF.large,
-            }}>
+            }}
+          >
             <BtnIcon
-              theme={'transparent'}
-              icon={!started ? 'person-walking-arrow-right' : 'hand'}
+              theme={"transparent"}
+              icon={!started ? "person-walking-arrow-right" : "hand"}
               size={48}
               iconColor={COLORS_DF.greenAgrayu}
               onPress={() => {
-                !started ? setStarted(true) : setStarted(false)
+                !started ? setStarted(true) : setStarted(false);
               }}
             />
             <BtnIcon
-              theme={'transparent'}
-              icon={'person-walking-arrow-loop-left'}
+              theme={"transparent"}
+              icon={"person-walking-arrow-loop-left"}
               size={48}
               iconColor={COLORS_DF.greenAgrayu}
               onPress={() => {
-                deletePoint()
+                deletePoint();
               }}
             />
           </View>
           <Btn
-            title={parcel[0].polygon ? 'Volver' : 'Guardar Polígono'}
+            title={parcel[0].polygon ? "Volver" : "Guardar Polígono"}
             onPress={parcel[0].polygon ? navigation.goBack : () => onSubmit()}
             theme="agrayu"
           />
         </View>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default GradientLineRecorrer
+export default GradientLineRecorrer;
