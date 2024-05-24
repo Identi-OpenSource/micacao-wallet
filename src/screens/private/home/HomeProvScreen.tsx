96d0c37eacb56e26dae6c7d7e07c3fa7df6b1885
jroman-identi <jroman@identi-digital.org>
2024-05-24 10:22:49 -0500

    feat: Blockchain recursive

--- src/screens/private/home/HomeProvScreen.tsx
+++ src/screens/private/home/HomeProvScreen.tsx
@@ -10,7 +10,7 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native'
-import {writeTransaction, newWallet} from '../../../OCC/occ'
+import {writeTransaction, newWallet, fundingWallet} from '../../../OCC/occ'
 import {imgFrame, imgLayer} from '../../../assets/imgs'
 import {LoadingSave} from '../../../components/loading/LoadinSave'
 import {SafeArea} from '../../../components/safe-area/SafeArea'
@@ -92,16 +92,14 @@ export const HomeProvScreen = () => {
 
   const getWallet = () => {
     // Create Wallet
-    //const wallet = JSON.parse(storage.getString('wallet') || '{}')
-    const wallet = {
-      isFunding: true,
-      wallet: {
-        walletOFC: 'R9vNe1TJLJta1srkqNo8WBrGN4JDJpTCDQ',
-        wif: 'L2e3T9u1ph4nceszGLqpCmwZ8soZf19hnonUNfiFywwL2bNADxwC',
-      },
-    }
+    // const wallet = {
+    //   isFunding: true,
+    //   wallet: {
+    //     walletOFC: 'R9vNe1TJLJta1srkqNo8WBrGN4JDJpTCDQ',
+    //     wif: 'L2e3T9u1ph4nceszGLqpCmwZ8soZf19hnonUNfiFywwL2bNADxwC',
+    //   },
+    // }
     console.log(wallet)
-    setWa(wallet.wallet)
 
     //Testing Wallet
     // const wallet = newWallet()
@@ -260,7 +258,7 @@ const Body = (props: {
           </Text>
         </TouchableOpacity>
 
-        <TouchableOpacity
+        {/* <TouchableOpacity
           style={[styles.bodyCard]}
           activeOpacity={0.9}
           onPress={() => getWallet()}>
@@ -276,7 +274,7 @@ const Body = (props: {
           <Text style={[styles.titleCard, syncUp && styles.filter]}>
             {'write Wallet'}
           </Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
       </View>
     </View>
   )
